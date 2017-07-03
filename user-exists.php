02f45c4fc7d332188a5c3590d9c19da024615104
Kun Ran <kun@lodestreams.com>
2017-07-03 17:13:24 +0800

    refactor: Change to checking username

--- user-exists.php
+++ user-exists.php
@@ -1,13 +1,13 @@
 <?php
 /**
  Plugin name: User Exists API Endpoint
- Version: 0.1.0
+ Version: 0.1.1
  */
 
 add_action( 'rest_api_init', function(){
 	register_rest_route( 'calderawp', '/user-exists', [
 		'args' => [
-			'email' => [
+			'username' => [
 				'type' => 'string',
 				'required' => true,
 			]
@@ -19,25 +19,25 @@ add_action( 'rest_api_init', function(){
 			 * Use to impliment throttling
 			 *
 			 * @param null|WP_Error Return a WP_Error to stop check and return that error via API
-			 * @param string $email Email address
+			 * @param string $username username address
 			 * @param WP_REST_Request $request  Current request
 			 */
-			$pre_serve = apply_filters( 'cwp_user_exists_pre_check', null,  $request[ 'email' ], $request );
+			$pre_serve = apply_filters( 'cwp_user_exists_pre_check', null,  $request[ 'username' ], $request );
 			if( is_wp_error( $pre_serve ) ){
 				return rest_ensure_response( $pre_serve );
 			}
 
-			if( is_email( $request[ 'email' ] ) && is_numeric( email_exists( $request[ 'email' ] ) ) ){
+			if( is_numeric( username_exists( $request[ 'username' ] ) ) ){
 				return rest_ensure_response( [ 'exists' => true ] );
 			}
 
 			/**
 			 * Fires when user exist check fails
 			 *
-			 * @param string$email Email address
+			 * @param string$username username address
 			 * @param WP_REST_Request $request  Current request
 			 */
-			do_action( 'cwp_user_exits_failed', $request[ 'email' ], $request );
+			do_action( 'cwp_user_exits_failed', $request[ 'username' ], $request );
 			return rest_ensure_response( [ 'exists' => false ] );
 
 		}
@@ -53,9 +53,9 @@ add_action( 'plugins_loaded', function(){
 		/**
 		 * Increment and count fails
 		 */
-		add_action( 'cwp_user_exits_failed', function( $email  ){
-			incrementFails( $email );
-			$fails = countFails( $email );
+		add_action( 'cwp_user_exits_failed', function( $username  ){
+			incrementFails( $username );
+			$fails = countFails( $username );
 			$settings = get_loginlockdownOptions();
 			$allowed = 3;
 			if( ! empty( $settings[ 'max_login_retries' ] ) ){
@@ -64,17 +64,17 @@ add_action( 'plugins_loaded', function(){
 
 
 			//To many fails, you are now locked out.
-			if( cwp_user_exist_is_to_many( $email ) ){
-				lockDown( $email );
+			if( cwp_user_exist_is_to_many( $username ) ){
+				lockDown( $username );
 			}
 		});
 
 		/**
 		 * Prevent user exists check if locked out
 		 */
-		add_filter( 'cwp_user_exists_pre_check', function( $return, $email ){
+		add_filter( 'cwp_user_exists_pre_check', function( $return, $username ){
 			//If locked out, return WP_Error
-			if( isLockedDown() ||cwp_user_exist_is_to_many( $email ) ){
+			if( isLockedDown() ||cwp_user_exist_is_to_many( $username ) ){
 				return new WP_Error( 'user-exists-lockout', __( 'Locked out' ) );
 			}
 
@@ -87,12 +87,12 @@ add_action( 'plugins_loaded', function(){
 	/**
 	 * Check if too many lockouts
 	 *
-	 * @param string $email Email to check
+	 * @param string $username username to check
 	 *
 	 * @return bool
 	 */
-	function cwp_user_exist_is_to_many( $email ){
-		$fails = countFails( $email );
+	function cwp_user_exist_is_to_many( $username ){
+		$fails = countFails( $username );
 		$settings = get_loginlockdownOptions();
 		$allowed = 3;
 		if( ! empty( $settings[ 'max_login_retries' ] ) ){
